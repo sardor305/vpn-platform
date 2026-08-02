4141c951ab39f62f886956f967211a5190ed6690
Sardor <mr.sarkor.94.sm@gmail.com>
2026-08-02 16:39:57 +0300

    Implement Marzban foundation

--- app/config/config.py
+++ app/config/config.py
@@ -4,13 +4,21 @@ from dotenv import load_dotenv
 load_dotenv()
 
 for key, value in os.environ.items():
-    if "DATABASE" in key or "BOT" in key:
+    if (
+        "DATABASE" in key
+        or "BOT" in key
+        or "MARZBAN" in key
+    ):
         print(repr(key), "=", repr(value))
 
 
 class Config:
     BOT_TOKEN = os.getenv("BOT_TOKEN")
     DATABASE_URL = os.getenv("DATABASE_URL")
+
+    MARZBAN_URL = os.getenv("MARZBAN_URL")
+    MARZBAN_USERNAME = os.getenv("MARZBAN_USERNAME")
+    MARZBAN_PASSWORD = os.getenv("MARZBAN_PASSWORD")
 
 
 config = Config()
--- app/services/marzban_service.py
+++ app/services/marzban_service.py
@@ -6,5 +6,27 @@ class MarzbanService:
     def __init__(
         self,
         client: MarzbanClient,
+        username: str,
+        password: str,
     ):
         self.client = client
+        self.username = username
+        self.password = password
+
+    async def login(self):
+
+        return await self.client.login(
+            username=self.username,
+            password=self.password,
+        )
+
+    async def create_user(
+        self,
+        user_data: dict,
+    ):
+
+        await self.login()
+
+        return await self.client.create_user(
+            user_data=user_data,
+        )
