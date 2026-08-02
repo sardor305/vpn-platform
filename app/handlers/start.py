8823dd98b844ed4530d6df5f3de21308f2e26f53
Sardor <mr.sarkor.94.sm@gmail.com>
2026-08-03 00:07:02 +0300

    Fix user transaction commit

--- app/handlers/start.py
+++ app/handlers/start.py
@@ -4,7 +4,6 @@ from aiogram.types import Message
 
 from app.database.database import async_session
 from app.services.user_service import UserService
-
 from app.keyboards.menu import main_menu
 
 router = Router()
@@ -15,20 +14,27 @@ async def start_handler(message: Message):
 
     async with async_session() as session:
 
-        user_service = UserService(session)
+        try:
+            user_service = UserService(session)
 
-        user, created = await user_service.get_or_create_user(
-            telegram_id=message.from_user.id,
-            username=message.from_user.username,
-            first_name=message.from_user.first_name,
-            last_name=message.from_user.last_name,
-            language_code=message.from_user.language_code,
-        )
+            user, created = await user_service.get_or_create_user(
+                telegram_id=message.from_user.id,
+                username=message.from_user.username,
+                first_name=message.from_user.first_name,
+                last_name=message.from_user.last_name,
+                language_code=message.from_user.language_code,
+            )
 
-        if created:
-            text = "🎉 Siz muvaffaqiyatli ro'yxatdan o'tdingiz!"
-        else:
-            text = "👋 Qaytganingizdan xursandmiz!"
+            await session.commit()
+
+        except Exception:
+            await session.rollback()
+            raise
+
+    if created:
+        text = "🎉 Siz muvaffaqiyatli ro'yxatdan o'tdingiz!"
+    else:
+        text = "👋 Qaytganingizdan xursandmiz!"
 
     await message.answer(
         f"{text}\n\n👋 Assalomu alaykum!\n\nVPN Platformaga xush kelibsiz.",
